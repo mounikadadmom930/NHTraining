46162547f4fb2f209062f644b940042b270928ac
mounikadadmom930 <mounikadadmom930@gmail.com>
2022-01-31 16:22:55 +0530

    day6 submission

---
+++ day6assiment/day6sum/da6assiment3/da6assiment3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace da6assiment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList data = new ArrayList();
+            int sum = 0;
+            data.Add(6);
+            data.Add(7);    
+            data.Add(8);
+            data.Add(9);
+            data.Add(10);
+
+            foreach(var d in data)
+            {
+                sum = sum + (int)d;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ day6assiment/string5values/day6assinment2/day6assinment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6assinment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> data = new List<string>();
+            data.Add("mounika");
+            data.Add("pavani");
+            data.Add("madavi");
+            data.Add("manju");
+            data.Add("latha");
+
+            for(int i=0;i<data.Count;i++)
+            {
+                Console.WriteLine(data[i]); 
+            }
+           //foreach loop
+           foreach(var d in data)
+                Console.WriteLine(d);
+
+           //lamda
+           data.ForEach(p=>Console.WriteLine(p));
+            Console.ReadLine();
+           
+        }
+    }
+}
