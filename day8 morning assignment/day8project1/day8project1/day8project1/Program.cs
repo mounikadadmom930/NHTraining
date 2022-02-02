8972dafc48f63f0dd3852bbeb0fd3e454ce2cd1c
mounikadadmom930 <mounikadadmom930@gmail.com>
2022-02-02 17:12:58 +0530

    day8 submission

---
+++ day8 morning assignment/day8project1/day8project1/day8project1/Program.cs
@@ -0,0 +1,43 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day8project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> data = new List<int>() { 45, 24, 46, 75, 89, 21, 34, 44 };
+            //even numbers using for loop
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i] % 2 == 0) 
+                Console.WriteLine(data[i]);
+            }
+
+            //even numbers using foreach loop
+            foreach (var d in data)
+            {
+                if (d % 2 == 0)
+                    Console.WriteLine(d);
+            }
+
+            //using lamda
+            data.ForEach(x => Console.WriteLine(x));
+
+            data.Where(d => d % 2 == 0).ToList().ForEach(d => Console.WriteLine(d));
+
+            //using LINQ
+            var result = from d in data
+                         where d % 2 == 0
+                         select d;
+            result.ToList().ForEach(d => Console.WriteLine());
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ day8 morning assignment/day8project5/day8project5/day8project5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8project5
+{
+    internal class Bank_1
+    {
+        class Bank
+        {
+            public int Id;
+            public string Name;
+            public int Income;
+        }
+
+        static void Main(string[] args)
+        {
+            /********************************
+             * Author:paluru mounika
+             * Purpose:bank class more than 600000
+             ********************************/
+
+            List<Bank> Bank = new List<Bank>()
+            {
+                new Bank() { Id = 1, Name = "Andra", Income = 400000 },
+
+                new Bank() { Id = 2, Name = "sbi", Income = 70000 },
+
+                new Bank() { Id = 3, Name = "hdfc", Income = 1000000 },
+
+                new Bank() { Id = 4, Name = "icic", Income = 30000 }
+            };
+            //Print the values using for loop
+
+            for (int i = 0; i < Bank.Count; i++)
+
+            {
+                if (Bank[i].Income >60000)
+                    Console.WriteLine($"Id={Bank[i].Id},Name={Bank[i].Name},Income={Bank[i].Income}");
+            }
+
+
+            //Print the values using foreah loop
+
+            foreach (var b in Bank)
+            {
+                if (b.Income >60009)
+                    Console.WriteLine($"Id={b.Id},Name={b.Name},income={b.Income}");
+            }
+
+            //Print values using Lambda Expression
+
+            Bank.ToList().Where(b => b.Income >60000).ToList().ForEach(b => Console.WriteLine($"Id={b.Id},Name={b.Name},income={b.Income}"));
+
+           //print the values using LINQ
+
+           var result=from b in Bank
+                      where b.Income>60000
+                      select b.Name;
+        result.ToList().ForEach(b => Console.WriteLine(b));
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
+
