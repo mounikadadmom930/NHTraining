4a3ea47d476cbf34dca16557c6dae267bcc3c4d3
mounikadadmom930 <mounikadadmom930@gmail.com>
2022-02-14 10:40:09 +0530

    print Helloworld

---
+++ Day16 assignment/day16 project1/day16project1/day16project1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day16project1
+{
+    class message
+    {
+        public string PrintHello()
+        {
+            return"Hello World";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            message m=new message();    
+            Console.WriteLine(m.PrintHello());
+            Console.ReadLine();
+        }
+    }
+}
