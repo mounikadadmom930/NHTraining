24783e5d4fa73543c4ae94bb6be73e5e449b2888
mounikadadmom930 <mounikadadmom930@gmail.com>
2022-02-15 12:21:35 +0530

    day17 submission

---
+++ day17 assignment/day17project1/day17project1/MounikaLibrary/MyBaseClass1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MounikaLibrary;
+//***************************************************
+//Author:paluru mounika
+//Purpose:Using Access modifiers
+//***********************************************************
+
+namespace MounikaLibrary
+{
+    /// <summary>
+    /// access modifiers with in class
+    /// </summary>
+    public class MyBaseClass1
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        int e;
+        protected internal int f;
+        public void MyBaseClass()
+        {
+            a = 11;
+            b = 25;  
+            c = 31;  
+            d = 43;  
+            e = 50;  
+            f = 61;  
+
+        }
+    }
+    /// <summary>
+    /// methode for MyDerivedClass
+    /// </summary>
+    public class MyDerivedClass:MyBaseClass1
+    {
+        public void MyDerivedClassMethod()
+        {
+            a = 5;
+            b = 7;
+            c = 3;
+            d = 8;
+            e = 9;  
+            f = 10; 
+
+            
+        }
+
+    }
+    /// <summary>
+    /// method for MyDerivedClass
+    /// </summary>
+    public class MyOtherClass
+    {
+        public void MyOtherClassMethod()
+        {
+            MyBaseClass1 mb=new MyBaseClass1(); 
+            mb.a = 5;   
+            mb.b = 2;   
+            mb.c = 4;   
+            mb.d = 3;   
+            mb.e = 1;   
+            mb.f = 6;   
+
+        }
+
+    }
+}
---
+++ day17 assignment/day17project1/day17project1/PublicLibrary/MyPublicLibraryDerivedClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MounikaLibrary;
+//***************************************
+//Author:paluru mounika
+//purpose:using Access modifiers
+//****************************************
+
+namespace PublicLibrary
+{
+    /// <summary>
+    /// using mypubliclibraryclass method
+    /// </summary>
+    public class MyPublicLibraryDerivedClass:MyBaseClass1
+    {
+        public void MyPublicLibraryDerivedClassMethod()
+        {
+            a = 3;
+            b = 5;
+            c = 6;  
+            d = 7;  
+            e = 8;  
+            f = 9;  
+
+        }
+    }
+    /// <summary>
+    /// using mypubliclibraryotherclass method
+    /// </summary>
+    public class MyPublicOtherClass:MyBaseClass1 
+    {
+        public void MyPublicOtherClassMethod()
+        {
+            MyBaseClass1 mb = new MyBaseClass1();
+            mb.a = 1;   
+            mb.b = 2;   
+            mb.c = 3;   
+            mb.d = 4;   
+            mb.e = 5;   
+            mb.f = 6;   
+
+        }
+    }
+}
