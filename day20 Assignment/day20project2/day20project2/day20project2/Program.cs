3c6986d3c6d042dd26320a70168536d121629e45
mounikadadmom930 <mounikadadmom930@gmail.com>
2022-02-19 15:16:14 +0530

    day20 submission

---
+++ day20 Assignment/day20project2/day20project2/day20project2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//**************************************************
+//Author: paluru mounika
+//Purpose: example C# code for multi cast delegate
+//***************************************************
+namespace project_2
+{
+    public delegate void MyCaller(int a, int b);
+    internal class Program
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public static void Div(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+        static void Main(string[] args)
+        {
+            MyCaller mc = new MyCaller(Add);
+            mc += Mul;
+            mc += Div;
+            // 15, 16
+            mc(15, 16);
+            Console.WriteLine("********************************************");
+            //13,16
+            mc(13, 16);
+            Console.WriteLine("*********************************************");
+            //14,18
+            mc(14, 18);
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ day20 Assignment/day20project3/day20project3/day20project3/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//*************************************************************
+//Author: paluru mounika
+//Purpose: WACP to illustrate nullable type
+//*************************************************************
+
+namespace project_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte? input = 10;
+            if (input.HasValue)
+                Console.WriteLine(input * input);
+            else
+                Console.WriteLine("No vaiue");
+            Console.ReadLine();
+        }
+    }
+}
