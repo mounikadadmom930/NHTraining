6df2a541c4ecbcc6af3b0134055e0f7ac60580a7
mounikadadmom930 <mounikadadmom930@gmail.com>
2022-02-22 20:53:40 +0530

    day 21 submission

---
+++ day22 assignments/day22 project1/MouniFinalProject/BusinessLogicLeyar/EmployeeBLL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+//**************************************
+//Author:paluru mounika
+//purpose:business logic layer
+//**************************************
+
+namespace BusinessLogicLeyar
+{
+    public class EmployeeBLL
+    {
+        /// <summary>
+        /// AddEmployee details
+        /// </summary>
+        /// <param name="empId"></param>
+        /// <param name="empname"></param>
+        /// <param name="empsalary"></param>
+        /// <param name="empAge"></param>
+        /// <returns></returns>
+        public static bool AddEmployee(int empId, string empname, int empsalary, int empAge)
+        {
+            var result = EmployeeDAL.AddEmployee(empId, empname, empsalary, empAge);
+
+
+            return result;
+        }
+        /// <summary>
+        /// get employee id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static List<string> GetEmployeeById(int id)
+        {
+            var result = EmployeeDAL.GetEmployeeById(id);
+            return result;
+        }
+        /// <summary>
+        /// get employee by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static List<string> GetEmployeeByname(string name)
+        {
+            var result = EmployeeDAL.GetEmployeeByname(name);
+            return result;
+        }
+        /// <summary>
+        /// display all emmployee details
+        /// </summary>
+        /// <returns></returns>
+        public static string[] GetallEmployees()
+        {
+            var result = EmployeeDAL.GetallEmployees();
+            return result;
+        }
+    }
+ }
---
+++ day22 assignments/day22 project1/MouniFinalProject/DataAccessLayer/EmployeeDAL.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static class EmployeeDAL
+    {
+        public  static string filepath = "C:\\Users\\mouni\\my html/EmployeeData";
+        public  static bool AddEmployee(int empId,string empname,int empsalary,int empAge)
+        {
+            try
+            {
+                string textcontent = string.Concat(empId, ",", empname, ",", empsalary, ",", empAge, ",");
+                File.AppendAllText(filepath, textcontent + Environment.NewLine);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static List<string> GetEmployeeById(int id)
+        {
+            var allEmployees = File.ReadAllLines(filepath);
+            bool isFound = false;
+
+            List<string> EmployeeFound = new List<string>();
+            foreach (string employee in allEmployees)
+            {
+                var empDetails = employee.Split(',');
+                if (Convert.ToInt32(empDetails[0]) == id)
+                {
+                    isFound = true;
+                    EmployeeFound.Add(employee);
+                    break;
+                }
+            }
+            return EmployeeFound;
+        }
+
+        public static List<string> GetEmployeeByname(string name)
+        {
+            var allEmployees = File.ReadAllLines(filepath);
+            bool isFound = false;
+
+            List<string> EmployeeFound = new List<string>();
+            foreach (string employee in allEmployees)
+            {
+                var empDetails = employee.Split(',');
+                if ((empDetails[1]).Contains(name))
+                {
+                   
+                    EmployeeFound.Add(employee);
+                  
+                }
+            }
+            return EmployeeFound;
+        }
+        public static string [] GetallEmployees()
+        {
+            var allEmployees=File.ReadAllLines(filepath);
+            return allEmployees;    
+            
+        }
+
+            }
+        }
+
+
